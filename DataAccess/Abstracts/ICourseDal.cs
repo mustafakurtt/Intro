7d19ac6fae31c7da79073b76d38f2df52f857265
mustafakurtt <mstfkurt07@gmail.com>
2024-02-18 13:41:17 +0300

    day 2

---
+++ DataAccess/Abstracts/ICourseDal.cs
@@ -0,0 +1,9 @@
+﻿using Intro.Entities;
+
+namespace Intro.DataAccess.Concretes;
+
+public interface ICourseDal
+{
+    List<Course> GetAll();
+    void Add(Course course);
+}
---
+++ DataAccess/Concretes/DapperCourseDal.cs
@@ -0,0 +1,16 @@
+﻿using Intro.Entities;
+
+namespace Intro.DataAccess.Concretes;
+
+public class DapperCourseDal : ICourseDal
+{
+    public List<Course> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(Course course)
+    {
+        throw new NotImplementedException();
+    }
+}
